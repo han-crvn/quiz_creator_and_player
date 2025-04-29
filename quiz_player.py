2e83ccaf4aae8682c59e582199c3b1b50b009aa2
Shane <palomoshanecraven@gmail.com>
2025-04-29 16:48:33 +0800

    Algorithm and Pseudocode for Quiz Player.

---
+++ quiz_player.py
@@ -0,0 +1,7 @@
+# Import libraries.
+
+# Allow users to choose from the options.
+
+# If users choose option 1, allow them to choose and answer a specific quiz.
+
+# If users choose option2, allow them to leave the program.
