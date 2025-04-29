49e9b6af6d9119bdff6770ec4ee53d10b60c94bc
Shane <palomoshanecraven@gmail.com>
2025-04-29 16:57:14 +0800

    For option (Play a Quiz), check if there is a category existing and list them down.

--- quiz_player.py
+++ quiz_player.py
@@ -18,15 +18,27 @@ while True:
     try:
         print("\nSelection:")
         print("1. Play a Quiz \n2. Exit \n")
+        
+        # Allow users to input their choice.
         choice = int(input("Enter the number of your choice: "))
 
         # If users choose option 1, allow them to choose and answer a specific quiz.
         if choice == 1:
-            pass
+            
+            # Check if there is category in file.
+            if not data:
+                print("\nThere are currently no categories.\n")
+                continue
+            
+            # List down the categories.
+            print("\nCategories: ")
+            for number, category in enumerate(data.keys(), 1):
+                print(f"{number}. {category}")
 
         # If users choose option2, allow them to leave the program.
         if choice == 2:
             break
-        
+    
+    # Catch invalid input.
     except ValueError:
         print("Invalid input! try again.")
