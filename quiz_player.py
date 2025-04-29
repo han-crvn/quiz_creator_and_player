9315bf4e081daa061b76ed54b683181966bf3e98
Shane <palomoshanecraven@gmail.com>
2025-04-29 17:16:51 +0800

    Access the question and choices.

--- quiz_player.py
+++ quiz_player.py
@@ -39,11 +39,25 @@ while True:
                 # Allow users to choose from categories.
                 selected_choice = int(input("\nEnter the number of the chosen category: "))
 
-                # Access the category
-                category_name = list(data.keys())[selected_choice -1]
-                
+                # Access the category.
+                category_name = list(data.keys())
+
+                if 1 <= selected_choice <= len(category_name):
+                    chosen = category_name[selected_choice - 1]
+                    question_set = data[chosen]
+
                 # Add short message.
                 print(f"{category_name} is successfully chosen.\n")
+                
+                # List down the question.
+                for number_question, question in enumerate(question_set, 1):
+                    print(f"{number}.) {question['question']}\n")
+
+                    # List down the choices.
+                    for number_choice, choices in question['choices'].items():
+                        print(f"{number_choice}. {choices}")
+
+                    
 
             #Catch invalid input.
             except ValueError:
