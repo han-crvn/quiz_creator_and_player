31f57eabf88115be6b8cc26722cc4f991f67e332
Shane <palomoshanecraven@gmail.com>
2025-04-29 16:53:20 +0800

    Add the libraries and variable needed.

--- quiz_player.py
+++ quiz_player.py
@@ -1,7 +1,21 @@
 # Import libraries.
+import os
+import json
+
+# File location of the categories.
+file_name = "categories.json"
+
+# Access the file.
+if os.path.exists(file_name):
+    with open(file_name, "r") as file:
+        data = json.load(file)
+
+else:
+    data = {}
 
 # Allow users to choose from the options.
 
 # If users choose option 1, allow them to choose and answer a specific quiz.
 
 # If users choose option2, allow them to leave the program.
+
