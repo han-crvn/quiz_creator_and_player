7b2d8b7ed5923fb9516eeb4aa8b61f2d62eaf614
Shane <palomoshanecraven@gmail.com>
2025-04-29 15:18:23 +0800

    Add options and allow users to choose from it, and catch invalid input.

--- quiz_creator.py
+++ quiz_creator.py
@@ -6,9 +6,24 @@ import json
 file_name = "categories.json"
 
 # Allow users to choose from the options.
+while True:
+    try:
+        print("\nSelection:")
+        print("1. Add category \n2. Access a category \n3. Exit \n")
+        choice = int(input("Enter the number of your choice: "))
 
-# If users choose option 1, allow them to add category.
-
-# If users choose option 2, allow them to access category and add question set.
-
-# If users choose option 3, allow them to exit the program.
+        # If users choose option 1, allow them to add category.
+        if choice == 1:
+            pass
+    
+        # If users choose option 2, allow them to access category and add question set.
+        elif choice == 2:
+            pass
+    
+        # If users choose option 3, allow them to exit the program.
+        elif choice == 3:
+            break
+    
+    # Catch invalid input.
+    except ValueError:
+        print("Invalid input! try again!")
