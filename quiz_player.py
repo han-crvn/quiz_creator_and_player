61f8b2e192defedfd6f08825f3cd709136e355d5
Shane <palomoshanecraven@gmail.com>
2025-04-29 16:55:15 +0800

    Allow users to choose from the options.

--- quiz_player.py
+++ quiz_player.py
@@ -14,8 +14,19 @@ else:
     data = {}
 
 # Allow users to choose from the options.
+while True:
+    try:
+        print("\nSelection:")
+        print("1. Play a Quiz \n2. Exit \n")
+        choice = int(input("Enter the number of your choice: "))
 
-# If users choose option 1, allow them to choose and answer a specific quiz.
+        # If users choose option 1, allow them to choose and answer a specific quiz.
+        if choice == 1:
+            pass
 
-# If users choose option2, allow them to leave the program.
-
+        # If users choose option2, allow them to leave the program.
+        if choice == 2:
+            break
+        
+    except ValueError:
+        print("Invalid input! try again.")
