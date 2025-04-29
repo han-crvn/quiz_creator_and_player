22ddea0140a3ce566bdedc48bf171b0b4fefcfea
Shane <palomoshanecraven@gmail.com>
2025-04-29 15:12:24 +0800

    Algorithm and Pseudocode for Quiz Creator.

---
+++ quiz_creator.py
@@ -0,0 +1,9 @@
+# Import Libraries.
+
+# Allow users to choose from the options.
+
+# If users choose option 1, allow them to add category.
+
+# If users choose option 2, allow them to access category and add question set.
+
+# If users choose option 3, allow them to exit the program.
